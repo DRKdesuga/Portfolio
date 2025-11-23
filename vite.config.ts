73972cc08785b646a20deb5999e64f024c32f2c9
hamza.hannat <hamza.hannat@epita.fr>
2025-11-24 00:33:47 +0100

    feat: changes for pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
+export default defineConfig({
+    base: '/Portfolio/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
@@ -51,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
     },
     server: {
       port: 3000,
